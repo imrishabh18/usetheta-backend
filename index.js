f876574dffcf08fa9a1ef934ee962af3c77b24d5
Rishabh Gupta <rishabhgupta@Rishabhs-MacBook-Pro.local>
2021-07-22 22:33:05 +0530

    Deploy 3

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({extended: true}))
 app.use(cors())
 
 const CONNECTION_URL = process.env.CONNECTION_URL
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on ${PORT}`)))
